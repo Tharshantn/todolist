870b88e78756b427257d0afa1aece97de0432fbf
Tharshan <134129740+Tharshantn@users.noreply.github.com>
2024-11-30 20:32:47 +0530

    Add files via upload

---
+++ todolist.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="todolist.aspx.cs" Inherits="Todolist.todolist" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="gvTasks" runat="server" AutoGenerateColumns="false" OnRowCommand="gvTasks_RowCommand" >
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                    <asp:BoundField DataField="Title" HeaderText="Title" />
+                    <asp:BoundField DataField="Status" HeaderText="Status" />
+                    <asp:TemplateField HeaderText="Actions">
+                        <ItemTemplate>
+                            <asp:Button ID="btnEdit" runat="server" CommandName="EditTask" CommandArgument='<%# Eval("Id") %>' Text="Edit" />
+                <asp:Button ID="btnDelete" runat="server" CommandName="DeleteTask" CommandArgument='<%# Eval("Id") %>' Text="Delete" />
+                  
+                <asp:Button ID="btnCompleteTask" runat="server" CommandName="CompleteTask" CommandArgument='<%# Eval("Id") %>' Text="Complete Task" />
+                <asp:Button ID="btnIncompleteTask" runat="server" CommandName="IncompleteTask" CommandArgument='<%# Eval("Id") %>' Text="Incomplete Task" />
+            </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            <br />
+            <asp:Panel ID="pnlAddTask" runat="server">
+                <asp:Label ID="lblTitle" runat="server" Text="Title:"></asp:Label>
+                <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox>
+                <br />
+                <asp:Button ID="btnAddTask" runat="server" Text="Add Task" OnClick="btnAddTask_Click" />
+            </asp:Panel>
+            <asp:Panel ID="pnlEditTask" runat="server" Visible="false">
+                <asp:Label ID="lblTaskId" runat="server" Text="Task ID:"></asp:Label>
+                <asp:TextBox ID="txtTaskId" runat="server" ReadOnly="true"></asp:TextBox>
+                <br />
+                <asp:Label ID="lblEditTitle" runat="server" Text="Title:"></asp:Label>
+                <asp:TextBox ID="txtEditTitle" runat="server"></asp:TextBox>
+                <br />
+                <asp:Button ID="btnUpdateTask" runat="server" Text="Update Task" OnClick="btnUpdateTask_Click" />
+                <asp:Button ID="btnCancelUpdate" runat="server" Text="Cancel" OnClick="btnCancelUpdate_Click" />
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ todolist.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Todolist
+{
+    public partial class todolist : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridData();
+            }
+        }
+
+        private void BindGridData()
+        {
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("sp_ReadTasks", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                gvTasks.DataSource = table;
+                gvTasks.DataBind();
+            }
+        }
+   
+        protected void btnAddTask_Click(object sender, EventArgs e)
+        {
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("sp_CreateTask", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Title", txtTitle.Text);
+                command.ExecuteNonQuery();
+                BindGridData();
+                txtTitle.Text = string.Empty;
+            }
+        }
+
+        protected void gvTasks_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditTask")
+            {
+                int taskId = Convert.ToInt32(e.CommandArgument);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("sp_ReadTask", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", taskId);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        txtTaskId.Text = reader["Id"].ToString();
+                        txtEditTitle.Text = reader["Title"].ToString();
+                    }
+                    reader.Close();
+                    pnlAddTask.Visible = false;
+                    pnlEditTask.Visible = true;
+                }
+            }
+            else if (e.CommandName == "DeleteTask")
+            {
+                int taskId = Convert.ToInt32(e.CommandArgument);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("sp_DeleteTask", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", taskId);
+                    command.ExecuteNonQuery();
+                    BindGridData();
+                }
+            }
+            if (e.CommandName == "CompleteTask")
+            {
+                int taskId = Convert.ToInt32(e.CommandArgument);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("sp_UpdateTaskStatus", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", taskId);
+                    command.Parameters.AddWithValue("@Status", true); // Set status to true when task is completed
+                    command.ExecuteNonQuery();
+                    BindGridData();
+                }
+            }
+            else if (e.CommandName == "IncompleteTask")
+            {
+                int taskId = Convert.ToInt32(e.CommandArgument);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("sp_UpdateTaskStatus", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", taskId);
+                    command.Parameters.AddWithValue("@Status", false); // Set status to false when task is not completed
+                    command.ExecuteNonQuery();
+                    BindGridData();
+                }
+            }
+        }
+
+        protected void btnUpdateTask_Click(object sender, EventArgs e)
+        {
+          
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("sp_UpdateTask", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Id", txtTaskId.Text);
+                command.Parameters.AddWithValue("@Title", txtEditTitle.Text);
+                command.ExecuteNonQuery();
+                BindGridData();
+                pnlAddTask.Visible = true;
+                pnlEditTask.Visible = false;
+            }
+        }
+
+        protected void btnCancelUpdate_Click(object sender, EventArgs e)
+        {
+            pnlAddTask.Visible = true;
+            pnlEditTask.Visible = false;
+        }
+
+        
+    }
+}
